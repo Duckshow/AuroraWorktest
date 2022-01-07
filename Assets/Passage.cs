e1e542f98215a2823f4071025c1dea91896296c1
Stefan Andersson <stf.andersson@gmail.com>
2022-01-07 10:50:25 +0100

    Gave Passage-colliders some tolerance

--- Assets/Passage.cs
+++ Assets/Passage.cs
@@ -22,7 +22,8 @@ public class Passage : RoomTile
             collider = GetComponent<BoxCollider>();
         }
 
-        collider.size = new Vector3Int(Room.MIN_ROOM_WIDTH, Room.DEFAULT_ROOM_HEIGHT, Room.MIN_ROOM_WIDTH);
+        const float TOLERANCE = 0.99f;
+        collider.size = new Vector3(Room.MIN_ROOM_WIDTH, Room.DEFAULT_ROOM_HEIGHT, Room.MIN_ROOM_WIDTH) * TOLERANCE;
         collider.center = new Vector3(0f, collider.size.y / 2, collider.size.z / 2);
     }
 }
